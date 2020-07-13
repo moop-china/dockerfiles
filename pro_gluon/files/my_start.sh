576559458a203b8a651fc5e952b7be0f5741f34b
shark_wang <longwind.wang@gmail.com>
2020-07-13 16:08:28 +0800

    add pro_gurobi

---
+++ pro_gluon/files/my_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source activate $ENV_NAME
+jupyter nbextension enable runtimer/main
+jupyter nbextension enable toc2/main
+jupyter nbextension enable execute_time/ExecuteTime
+
+# jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.notebook_dir=$HOME --no-browser
+/usr/local/bin/start-notebook.sh --ip=0.0.0.0 --port=8888 --NotebookApp.notebook_dir=$HOME --no-browser
--- pro_jinrong/files/my_start.sh
+++ pro_jinrong/files/my_start.sh
@@ -2,5 +2,6 @@
 source activate $ENV_NAME
 jupyter nbextension enable runtimer/main
 jupyter nbextension enable toc2/main
+jupyter nbextension enable python-markdown/main
 # jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.notebook_dir=$HOME --no-browser
 /usr/local/bin/start-notebook.sh --ip=0.0.0.0 --port=8888 --NotebookApp.notebook_dir=$HOME --no-browser
